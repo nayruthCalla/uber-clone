9d897bb3be2fbbf8b8f2aae4a1eb4cd0bce62b64
nayruthCalla <nayruth.calla@gmail.com>
2022-03-29 18:04:35 -0500

    feat: Initial config of eslint and prettier

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Hola mundo</h1>
+      <h1> Hola mundo</h1>
     </div>
   );
 }
